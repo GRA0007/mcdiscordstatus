aea9f99f487beaff5085c03aae9c97a47fbf1be1
Ben Grant <benjamin.grantGRA0007@gmail.com>
2022-01-04 00:38:46 +1100

    Update user display

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-const Discord = require('discord.js');
-const fetch = require('node-fetch');
+const Discord = require('discord.js')
+const fetch = require('node-fetch')
 
-const client = new Discord.Client();
-const settings = require('./config.json');
-freq = settings.pingInterval * 1000;
+const client = new Discord.Client()
+const settings = require('./config.json')
+freq = settings.pingInterval * 1000
 
 const getServerStatus = () => {
 	fetch(`https://api.mcsrvstat.us/2/${settings.ip}`)
 		.then(res => res.json())
 		.then(status => {
 			if (!status.online) {
-				client.user.setStatus('dnd');
-				client.user.setActivity('Server offline', { type: 'WATCHING' });
-				return;
+				client.user.setStatus('dnd')
+				client.user.setActivity('Server offline', { type: 'WATCHING' })
+				return
 			}
 
 			if (status.players.online == 0) {
-				client.user.setStatus('idle');
-				client.user.setActivity(`\n⛏️ ${status.players.online} / ${status.players.max}`, { type: 'WATCHING' });
+				client.user.setStatus('idle')
+				client.user.setActivity(`\n⛏️ ${status.players.online} / ${status.players.max}`, { type: 'WATCHING' })
 			} else {
-				client.user.setStatus('online');
-				client.user.setActivity(`\n⛏️ ${status.players.online} / ${status.players.max}⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n\n👥 ${status.players.list.join(', ')}`, { type: 'WATCHING' });
+				client.user.setStatus('online')
+				client.user.setActivity(`\n⛏️ ${status.players.online} / ${status.players.max} (${status.players.list.join(', ')})`, { type: 'WATCHING' })
 			}
 		})
 		.catch(e => {
-			client.user.setStatus('dnd');
-			client.user.setActivity('API error', { type: 'WATCHING' });
-		});
-};
+			client.user.setStatus('dnd')
+			client.user.setActivity('API error', { type: 'WATCHING' })
+		})
+}
 
-client.on("ready", () => {
-	console.log(`Logged in as ${client.user.tag}!`);
-	getServerStatus();
-	client.setInterval(getServerStatus, freq);
-});
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`)
+	getServerStatus()
+	client.setInterval(getServerStatus, freq)
+})
 
-client.login(settings.token);
+client.login(settings.token)
